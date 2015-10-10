7e8db1354ebaca76e23f3253c9783d7c9aa0a5e8
Daniel Yak <danielyaa5@gmail.com>
2015-10-10 19:47:14 -0400

    added more code for game_room

--- modules/games/server/controllers/game_rooms.server.controller.js
+++ modules/games/server/controllers/game_rooms.server.controller.js
@@ -1,6 +1,73 @@
 'use strict';
-console.log('fuck');
-var dogs = ['german', 'maltese', 'beagel'];
-for(var dog of dogs) {
-	console.log(dog);
+
+function createGameSocket() {
+    return {
+        kickPlayer: function(player) {
+            console.log('kicked', player);
+        }
+    };
 }
+
+// Generate a random id for the game_room
+var generateID = function() {
+    var id_length = 8;
+    var timestamp = new Date();
+
+    // number of milliseconds since 1970
+    return timestamp.valueOf();
+};
+
+class PublicGameRoom {
+
+    constructor(players) {
+        // Set players prop, return error if not array
+        if (Array.isArray(players)) this.players = players;
+        else throw 'Attempted to create game without correct players param';
+
+        // Available game states
+        this.game_states = ['ESTABLISHED', 'START', 'DRAW', 'OVER', 'CHOOSE_WINNER', 'DISPLAY_WINNER', 'TERMINATED'];
+        this.game_state = this.game_states[0];
+        // Set min and max players
+        this.min_players = 3;
+        this.max_players = 8;
+        // Set the first judge
+        this.judge = players[0];
+        // Create and set a time id for game_room
+        this._id = generateID();
+        // Create a socket channel for game_room and subscribe players
+        this.game_socket = createGameSocket(this.players, this.playerDisconnects);
+    }
+
+    set gameState(state) {
+        this.game_state = state;
+    }
+
+    get gameState() {
+        return this.game_state;
+    }
+
+    // When a player presses leave game
+    playerExits(playerToRemove) {
+        // Delete player from players array
+        this.players.splice(this.players.indexOf(playerToRemove), 1);
+
+        // If theres not enough players to continue, terminate game
+        if (this.players.length < this.min_players) this.game_state = this.game_states[6];
+        // Else disconnect the player from the game socket
+        else this.game_socket.kickPlayer(playerToRemove);
+    }
+
+    // When a player disconnects without warning, e.g. closes window
+    playerDisconnects(playerToRemove) {
+        // Delete player from players array
+        this.players.splice(this.players.indexOf(playerToRemove), 1);
+        // If theres not enough players to continue, terminate game
+        if (this.players.length < this.min_players) this.game_state = this.game_states[6];
+    }
+}
+
+var x = new PublicGameRoom([1, 2, 3]);
+console.log(x.gameState);
+console.log(x.players);
+x.playerExits(1);
+console.log(generateID());
--- modules/games/server/controllers/games.server.controller.js
+++ modules/games/server/controllers/games.server.controller.js
@@ -17,40 +17,40 @@
 //         delete Games[this._id];
 //     };
 
-//     // When a player presses leave game
-//     this.playerExits = function(playerToRemove) {
-//         // Delete player from players array
-//         this.players.splice(this.players.indexOf(playerToRemove),1);
+    // // When a player presses leave game
+    // this.playerExits = function(playerToRemove) {
+    //     // Delete player from players array
+    //     this.players.splice(this.players.indexOf(playerToRemove),1);
 
-//         // If less than min players
-//         if (this.players.length < minPlayers) this.killGame();
+    //     // If less than min players
+    //     if (this.players.length < minPlayers) this.killGame();
 
-//         // If less than max players
-//         if (this.players.length < maxPlayers) {
-//                 this.needsPlayers = true;
-//         }
+    //     // If less than max players
+    //     if (this.players.length < maxPlayers) {
+    //             this.needsPlayers = true;
+    //     }
 
-//        gameSockets.kickPlayer(playerToRemove);
-//     };
+    //    gameSockets.kickPlayer(playerToRemove);
+    // };
     
-//     // When a player disconnects without warning, e.g. closes window
-//     this.playerDisconnects = function(playerToRemove) {
-//         // Delete player from players array
-//         this.players.splice(this.players.indexOf(playerToRemove),1);
+    // // When a player disconnects without warning, e.g. closes window
+    // this.playerDisconnects = function(playerToRemove) {
+    //     // Delete player from players array
+    //     this.players.splice(this.players.indexOf(playerToRemove),1);
 
-//         // If less than min players
-//         if (this.players.length < minPlayers) this.killGame();
+    //     // If less than min players
+    //     if (this.players.length < minPlayers) this.killGame();
 
-//         // If less than max players
-//         if (this.players.length < maxPlayers) {
-//                 this.needsPlayers = true;
-//         }
-//     };
+    //     // If less than max players
+    //     if (this.players.length < maxPlayers) {
+    //             this.needsPlayers = true;
+    //     }
+    // };
 
-//     this.selectJudges = function() {
-//         this.judge = this.players.pop();
-//         this.players = this.players.unshift(this.judge);
-//     };
+    // this.selectJudges = function() {
+    //     this.judge = this.players.pop();
+    //     this.players = this.players.unshift(this.judge);
+    // };
 
 //     this.setWinner = function(winner) {
 //         this.winner = winner;
