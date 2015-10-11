ecfecf5dc69cbfffeac604376cb80713f5fd1e8c
Daniel Yak <danielyaa5@gmail.com>
2015-10-10 20:34:20 -0400

    added new judge method

--- modules/games/server/controllers/game_rooms.server.controller.js
+++ modules/games/server/controllers/game_rooms.server.controller.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+Helper Functions
+*/
 function createGameSocket() {
     return {
         kickPlayer: function(player) {
@@ -17,11 +20,38 @@ var generateID = function() {
     return timestamp.valueOf();
 };
 
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 class PublicGameRoom {
 
     constructor(players) {
         // Set players prop, return error if not array
-        if (Array.isArray(players)) this.players = players;
+        if (Array.isArray(players)) this.players = shuffle(players);
         else throw 'Attempted to create game without correct players param';
 
         // Available game states
@@ -63,6 +93,12 @@ class PublicGameRoom {
         this.players.splice(this.players.indexOf(playerToRemove), 1);
         // If theres not enough players to continue, terminate game
         if (this.players.length < this.min_players) this.game_state = this.game_states[6];
+    }
+
+    // Sets a new judge by popping the end player
+    newJudge () {
+        this.judge = this.players.pop();
+        this.players = this.players.unshift(this.judge);
     }
 }
 
