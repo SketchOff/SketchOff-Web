0a7d49f41dca3a08c58a703260a45164c16d3dc6
Daniel Yak <danielyaa5@gmail.com>
2015-10-12 11:50:44 -0400

    added client folder to games modules

---
+++ modules/games/server/sockets/games.server.socket.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Player connect to game, if game state is establishing
+// Wait until all players connect before changing the games state to start
+
+
+// Create the game socket.io configuration
+export default function(io, socket) {
+	
+}
