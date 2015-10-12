ec721228bedc31c14556f7ec14efbb16583b037b
Daniel Yak <danielyaa5@gmail.com>
2015-10-12 11:58:17 -0400

    added join game button for testing

--- modules/games/client/controllers/games.client.controller.js
+++ modules/games/client/controllers/games.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Games controller
-angular.module('games').controller('ArticlesController', ['$scope', 'Authentication',
+angular.module('games').controller('GamesController', ['$scope', 'Authentication',
   function ($scope, $stateParams, $location, Authentication) {
     $scope.authentication = Authentication;
 
