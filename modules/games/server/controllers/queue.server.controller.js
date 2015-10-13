f448fbe368c6ea4aa8fdfee63885ad83c7e83de7
Daniel Yak <danielyaa5@gmail.com>
2015-10-13 02:50:15 -0400

    updated add player function in queue.server.controller

--- modules/games/server/controllers/queue.server.controller.js
+++ modules/games/server/controllers/queue.server.controller.js
@@ -12,24 +12,22 @@ export function addPlayer(player) {
     // add player to queue
     queue.push(player);
 
-    // available game rooms and players, start filling
+    // available game rooms, start filling
     var count = 0;
-    while (availableGameRooms.length && queue.length) {
+    while (availableGameRooms.length) {
 		var CurrRoom = availableGameRooms[count];
         
         // remove game room if its not in selecting winner or ending state or is full
         if (CurrRoom.getStateName() !== 'SELECTING_WINNER' || CurrRoom.getStateName() !== 'ENDING' || CurrRoom.isFull) {
         	availableGameRooms.splice(count, 1);
-        } else {
-            // fill game room until full and then remove
-            while (!CurrRoom.isFull) {
-                availableGameRooms[count].addPlayer(queue.pop());
-            }
-        	availableGameRooms.splice(count, 1);
+        } 
+        // add player to game room
+        else {
+            availableGameRooms[count].addPlayer(queue.pop());
         }
         count++;
     }
-    
+
     // if queue has enough players, 
     if (queue.length >= min_players) {
 
