55c27131cd530ecad4ceb944ec679b9fe1428e5d
Daniel Yak <danielyaa5@gmail.com>
2015-10-23 16:22:41 -0400

    got a working spinner

--- config/assets/default.js
+++ config/assets/default.js
@@ -17,8 +17,8 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/angular-spinner/angular-spinner.js',
-        'public/lib/spin/spin.js'
+        'public/lib/spin.js/spin.js',
+        'public/lib/angular-spinner/angular-spinner.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
