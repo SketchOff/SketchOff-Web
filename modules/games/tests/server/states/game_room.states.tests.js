4af9c273cc82d5fb0b4a730295a207c35c9bcfee
Daniel Yak <danielyaa5@gmail.com>
2015-11-07 12:13:51 -0500

    added another test to the game room states tests.

--- modules/games/tests/server/states/game_room.states.tests.js
+++ modules/games/tests/server/states/game_room.states.tests.js
@@ -1,43 +1,55 @@
-'use strict';
+    'use strict';
 
-import GameRoom from '../../../server/controllers/game_room.server.controller';
-import {max_players, min_players} from '../../../server/controllers/game_room.server.controller';
-import {
-    q
-}
-from '../../../server/controllers/queue.server.controller';
-
-var should = require('should');
-var SocketMock = require('socket-io-mock');
-var socketPort = 3006;
-var socketURL = 'http://0.0.0.0:';
-var ioserver = require('socket.io').listen(socketPort);
-q.setIO(ioserver);
-
-var getMockSockets = function(num_players) {
-    var sockets = [];
-    for (var i = 0; i < num_players; i++) {
-        sockets[i] = new SocketMock();
-        sockets[i].request = {
-            user: {
-                username: 'user' + i
-            }
-        };
+    import GameRoom from '../../../server/controllers/game_room.server.controller';
+    import {max_players, min_players} from '../../../server/controllers/game_room.server.controller';
+    import {
+        q
     }
-    return sockets;
-};
+    from '../../../server/controllers/queue.server.controller';
 
-describe('Game Room States Tests\n', function() {
-    // initialize a game room
+    var should = require('should');
+    var SocketMock = require('socket-io-mock');
+    var socketPort = 3006;
+    var socketURL = 'http://0.0.0.0:';
+    var ioserver = require('socket.io').listen(socketPort);
+    q.setIO(ioserver);
 
-    describe('Game Room Constructor Tests\n', function() {
-        var TheGameRoom, players;
+    var getMockSockets = function(num_players) {
+        var sockets = [];
+        for (var i = 0; i < num_players; i++) {
+            sockets[i] = new SocketMock();
+            sockets[i].request = {
+                user: {
+                    username: 'user' + i
+                }
+            };
+        }
+        return sockets;
+    };
 
-        it('Should be able to create a game room with max number of players', function(done) {
-        	players = getMockSockets(max_players);
-        	TheGameRoom = new GameRoom(players, true, 'randomly_generated_id');
-        	TheGameRoom.getNumPlayers().should.be.exactly(max_players);
-        	done();
+    describe('Game Room States Tests', function() {
+        // initialize a game room
+
+        describe('Public Game Room - Max Number Player', function() {
+            var TheGameRoom, players;
+
+            it('Should be able to create a game room with max number of players', function(done) {
+            	players = getMockSockets(max_players);
+            	TheGameRoom = new GameRoom(players, true, 'randomly_generated_id');
+            	TheGameRoom.getNumPlayers().should.be.exactly(max_players);
+            	done();
+            });
+
+            it('Should be in the ESTABLISHING state after GameRoom initialization', function(done) {
+                TheGameRoom.getStateName().should.be.exactly('ESTABLISHING');
+                done();
+            }); 
+
+            it('Should have a game_room_id property equal to the room\'s ID for every socket', function(done) {
+                for (let socket of players) {
+                    socket.game_room_id.should.be.exactly('randomly_generated_id');
+                }
+                done();
+            });
         });
     });
-});
