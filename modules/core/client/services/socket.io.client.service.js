c7eae56352919b0f86d24469c712af721baa1379
Daniel Yak <danielyaa5@gmail.com>
2015-11-19 11:50:09 -0500

    check if socket exists before adding listener

--- modules/core/client/services/socket.io.client.service.js
+++ modules/core/client/services/socket.io.client.service.js
@@ -2,52 +2,54 @@
 
 // Create the Socket.io wrapper service
 angular.module('core').service('Socket', ['Authentication', '$state', '$timeout',
-  function (Authentication, $state, $timeout) {
-    // Connect to Socket.io server
-    this.connect = function () {
-      // Connect only when authenticated
-      if (Authentication.user) {
-        this.socket = io();
-      }
-    };
-    this.connect();
+    function(Authentication, $state, $timeout) {
+        // Connect to Socket.io server
+        this.connect = function() {
+            // Connect only when authenticated
+            if (Authentication.user) {
+                this.socket = io();
+            }
+        };
+        this.connect();
 
-    this.socket.on('invite notification', function(msg){
-	console.log('invite notification');
-	console.log(msg);
-	if(confirm('Invite to', msg[0])){
-	    this.emit('join private lobby', [msg[0], msg[2]]);
-        }
-    });
-
-    this.socket.on('joined private lobby', function(){
-	console.log('joined private lobby');
-	$state.go('games.private-lobby');
-    });
-
-    // Wrap the Socket.io 'on' method
-    this.on = function (eventName, callback) {
-      if (this.socket) {
-        this.socket.on(eventName, function (data) {
-          $timeout(function () {
-            callback(data);
+        if (this.socket) {
+          this.socket.on('invite notification', function(msg) {
+              console.log('invite notification');
+              console.log(msg);
+              if (confirm('Invite to', msg[0])) {
+                  this.emit('join private lobby', [msg[0], msg[2]]);
+              }
           });
-        });
-      }
-    };
 
-    // Wrap the Socket.io 'emit' method
-    this.emit = function (eventName, data) {
-      if (this.socket) {
-        this.socket.emit(eventName, data);
-      }
-    };
+          this.socket.on('joined private lobby', function() {
+              console.log('joined private lobby');
+              $state.go('games.private-lobby');
+          });
+        }
 
-    // Wrap the Socket.io 'removeListener' method
-    this.removeListener = function (eventName) {
-      if (this.socket) {
-        this.socket.removeListener(eventName);
-      }
-    };
-  }
+        // Wrap the Socket.io 'on' method
+        this.on = function(eventName, callback) {
+            if (this.socket) {
+                this.socket.on(eventName, function(data) {
+                    $timeout(function() {
+                        callback(data);
+                    });
+                });
+            }
+        };
+
+        // Wrap the Socket.io 'emit' method
+        this.emit = function(eventName, data) {
+            if (this.socket) {
+                this.socket.emit(eventName, data);
+            }
+        };
+
+        // Wrap the Socket.io 'removeListener' method
+        this.removeListener = function(eventName) {
+            if (this.socket) {
+                this.socket.removeListener(eventName);
+            }
+        };
+    }
 ]);
