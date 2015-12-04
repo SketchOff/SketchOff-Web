a88a80cef071307b803786cccc76a2c74b1f4bee
Daniel Yak <danielyaa5@gmail.com>
2015-12-04 17:30:39 -0500

    basic game view ui

--- modules/games/client/controllers/game-view.client.controller.js
+++ modules/games/client/controllers/game-view.client.controller.js
@@ -2,14 +2,21 @@
 
 // Articles controller
 angular.module('games').controller('GameViewController', ['$scope', '$stateParams', 'Authentication', 'Games',
-  function ($scope, $stateParams, Authentication, Games) {
-    $scope.authentication = Authentication;
+    function($scope, $stateParams, Authentication, Games) {
+        $scope.authentication = Authentication;
+        $scope.flags = {
+            ready: false
+        };
 
-    // Find existing Article
-    $scope.findOne = function () {
-      $scope.game = Games.get({
-        gameId: $stateParams.gameId
-      });
-    };
-  }
+        // Find existing Article
+        $scope.findOne = function() {
+            $scope.game = Games.get({
+                gameId: $stateParams.gameId
+            }, function(game) {
+                $scope.flags.ready = true;
+                console.log('flags ready', $scope.flags.ready);
+            });
+        };
+
+    }
 ]);
